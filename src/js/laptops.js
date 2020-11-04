7e6a242e0be7b2b6e9b3a6e4a6f68475863c2780
Bumpfel <eric.enoksson@gmail.com>
2020-11-04 08:51:49 +0100

    chore: changed laptop objects to using constructor

--- src/js/laptops.js
+++ src/js/laptops.js
@@ -1,27 +1,36 @@
-export const laptops = [
-  {
-    name: "Kompak Derperon",
-    price: 9001,
-    features: "Slightly over-priced computer for the budget aware consumer.",
-    description: "The price might be over 9000, but it does come with a gift certificate for a free cup of coffee and a biscuit.",
-    img: "kompak.jpg"
-  }, {
-    name: "IBN AWD K2",
-    price: 8611,
-    features: "Comes with built in protection if dropped on the ground. Just make sure it's your own laptop you drop.",
-    description: "It's a beast from every perspective, even when it comes to weight and sound!",
-    img: "pc.jpg"
-  }, {
-    name: "Mak & Cheese OS Y",
-    price: 27900,
-    features: "Expensive, one button mouse. Never underestimate the benefits of dumbing down. You won't be able to afford anti-virus, but on the other hand, you probably won't need one.",
-    description: "This brand is known for something known as vendor lock-in. Usually this means communication between multiple devices works very well if you continue buying products of the same brand, but can make the buyer suicidal in other cases.",
-    img: "mak.png"
-  }, {
-    name: "9000 SUX",
-    price: 18000,
-    features: "Are you sure you're getting a good deal?",
-    description: "Buy one for two - special price for you! Hurry hurry hurry!",
-    img: "9000-sux.jpg"
-  }
-]
+export const laptops = []
+
+laptops.push(
+  new Laptop("Kompak Derperon",
+    9001,
+    "Slightly over-priced computer for the budget aware consumer.",
+    "The price might be over 9000, but it does come with a gift certificate for a free cup of coffee and a biscuit.",
+    "kompak.jpg"
+  ), new Laptop(
+    "IBN AWD K2",
+    8611,
+    "Comes with built in protection if dropped on the ground. Just make sure it's your own laptop you drop.",
+    "It's a beast from every perspective, even when it comes to weight and sound!",
+    "pc.jpg"
+  ), new Laptop(
+    "Mak & Cheese OS Y",
+    27900,
+    "Expensive, one button mouse. Never underestimate the benefits of dumbing down. You won't be able to afford anti-virus, but on the other hand, you probably won't need one.",
+    "This brand is known for something known as vendor lock-in. Usually this means communication between multiple devices works very well if you continue buying products of the same brand, but can make the buyer suicidal in other cases.",
+    "mak.png"
+  ), new Laptop(
+    "9000 SUX",
+    18000,
+    "Are you sure you're getting a good deal?",
+    "Buy one for two - special price for you! Hurry hurry hurry!",
+    "9000-sux.jpg"
+  )
+)
+
+function Laptop(name, price, features, description, img) {
+  this.name = name
+  this.price = price
+  this.features = features
+  this.description = description
+  this.img = img
+}
