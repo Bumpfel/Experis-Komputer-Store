82613f040b589f68bf8f3b5d73d747faba34068b
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2020-11-04 08:13:08 +0100

    feat: display laptop info + partial button functionality
    can "work", transfer pay to bank, display loan popup

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,86 @@
 import { laptops } from './laptops.js'
 
-let bankBalance = 0, payBalance = 0, hasLoan = false
+let bankBalance = 0, payBalance = 0, hasLoan = false, selectedLaptop
 
-const dropdown = document.querySelector('#laptopsDropdown')
+const alertTypes = { info: 'alert-info', success: 'alert-sucess', error: 'alert-danger'}
+
+const messageContainer = document.querySelector('#messages')
+
+const bankBalanceContainer = document.querySelector('#bankBalance')
+const payBalanceContainer = document.querySelector('#payBalance')
+
+const selectedProduct = document.querySelector('#selectedProduct')
+const selectedProductFeatures = document.querySelector('#features')
+const selectedProductImg = selectedProduct.querySelector('img')
+const selectedProductTitle = selectedProduct.querySelector('#title')
+const selectedProductDescription = selectedProduct.querySelector('#description')
+const selectedProductPrice = selectedProduct.querySelector('#price')
+
+const laptopsDropdown = document.querySelector('#laptopsDropdown')
 for(const laptop of laptops) {
   const option = document.createElement('option')
   option.text = laptop.name
-  dropdown.appendChild(option)
+  laptopsDropdown.appendChild(option)
 }
-dropdown.addEventListener('change', e => {
-  const found = laptops.find(laptop => laptop.name == dropdown.value)
-  console.log(found)
-  
+
+// Event Listeners
+laptopsDropdown.addEventListener('change', e => {
+  setSelectedLaptop(laptopsDropdown.value)
 })
+
+document.querySelector('#workButton').addEventListener('click', () => {
+  work()
+})
+
+document.querySelector('#bankButton').addEventListener('click', () => {
+  transferFunds()
+})
+
+document.querySelector('#loanForm').addEventListener('submit', () => {
+  const amount = loanForm.querySelector('#loanAmount').value
+  getLoan(amount)
+})
+
+
+// Functions
+const setSelectedLaptop = (laptop) => {
+  selectedLaptop = laptops.find(laptop => laptop.name == laptopsDropdown.value)
+  
+  selectedProductFeatures.innerText = selectedLaptop.features
+  selectedProductImg.src = 'assets/' + selectedLaptop.img
+  selectedProductTitle.innerText = selectedLaptop.name
+  selectedProductDescription.innerText = selectedLaptop.description
+  selectedProductPrice.innerText = selectedLaptop.price + ' SEK'
+}
 
 const work = () => {
   payBalance += 100
+  payBalanceContainer.innerText = payBalance + ' kr'
 }
 
 const transferFunds = () => {
   bankBalance += payBalance
   payBalance = 0
+  payBalanceContainer.innerText = payBalance + ' kr'
+  bankBalanceContainer.innerText = bankBalance + ' kr'
+}
+
+const alert = (msg, alertType) => {
+  messageContainer.innerText
 }
 
 const getLoan = (amount) => {
   if(amount * 2 > bankBalance) {
-
+    alert('You do not have sufficient funds to get this loan (basically we don\'t trust poor ppl', alertTypes.info)
+  } else if(amount <= 0) {
+    alert('Amount must be over 0', alertTypes.error)
   } else {
     bankBalance += amount
+    alert('Loan granted', alertTypes.success)
     hasLoan = true
   }
 }
+
+
+// Set default value
+setSelectedLaptop(laptops[0])
--- src/js/laptops.js
+++ src/js/laptops.js
@@ -1,22 +1,26 @@
 export const laptops = [
   {
-    name: "Intol Derperon",
+    name: "Kompak Derperon",
     price: 9001,
-    description: "The price might be over 9000, but it does come with a gift certificate for a free cup of coffee and a biscuit",
-    img: "intol-derperon.jpg"
+    features: "Slightly over-priced computer for the budget aware consumer.",
+    description: "The price might be over 9000, but it does come with a gift certificate for a free cup of coffee and a biscuit.",
+    img: "kompak.jpg"
   }, {
-    name: "AWD K2",
+    name: "IBN AWD K2",
     price: 8611,
-    description: "Is it a laptop? Is it a mountain? Does it require all wheel drive to get there? Order one and find out!",
-    img: "awd-k2.jpg"
+    features: "Comes with built in protection if dropped on the ground. Just make sure it's your own laptop you drop.",
+    description: "It's a beast from every perspective, even when it comes to weight and sound!",
+    img: "pc.jpg"
   }, {
     name: "Mak & Cheese OS Y",
     price: 27900,
-    description: "It's expensive, yes. The mouse only have one button, yes. But never underestimate the benefits of dumbing down. You will likely not break this thing unless you throw it out of a window",
-    img: "mak.jpg"
+    features: "Expensive, one button mouse. Never underestimate the benefits of dumbing down. You won't be able to afford anti-virus, but on the other hand, you probably won't need one.",
+    description: "This brand is known for something known as vendor lock-in. Usually this means communication between multiple devices works very well if you continue buying products of the same brand, but can make the buyer suicidal in other cases.",
+    img: "mak.png"
   }, {
     name: "9000 SUX",
-    price: "18000",
+    price: 18000,
+    features: "Are you sure you're getting a good deal?",
     description: "Buy one for two - special price for you! Hurry hurry hurry!",
     img: "9000-sux.jpg"
   }
